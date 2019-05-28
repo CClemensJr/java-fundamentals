8448d0fed52486a89c5067231badd565f6cd01c0
Charles Clemens <cclemensjr@gmail.com>
2019-05-27 23:59:47 -0700

    Added Main class and method

---
+++ basics/Main.java
@@ -0,0 +1,10 @@
+import java.time.localDateTime;
+
+public class Main {
+    public static void main(String[] args) {
+        //call pluralize
+        //call flipNHeads
+        //call clock
+    }
+
+}
