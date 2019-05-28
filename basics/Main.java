35afa1aa7b923fb3474e7f14e332597eb66e3531
Charles Clemens <cclemensjr@gmail.com>
2019-05-28 10:57:12 -0700

    Create LocalDateTime object

--- basics/Main.java
+++ basics/Main.java
@@ -1,4 +1,4 @@
-//import java.time.localDateTime;
+import java.time.LocalDateTime;
 
 public class Main {
     public static void main(String[] args) {
@@ -17,18 +17,31 @@ public class Main {
 
         //call clock
         clock();
-
-        System.out.println("Allo, love!");
     }
 
-    // Accept a string and a number and pluralize the string based on the number
+
+    /******************
+     * PLURALIZE
+     * Accept a string and a number and pluralize the string based on the number
+     * @param word
+     * @param number
+     * @return String
+     */
+
     public static String pluralize(String word, int number) {
         String pluralWord = number == 0 || number > 1 ? word + "s" : word;
 
         return pluralWord;
     }
 
-    // Take a number and simulate a coin flip until heads has appeared that number of times.
+
+
+    /******************
+     * FLIP N HEADS
+     * Take a number and simulate a coin flip until heads has appeared that number of times.
+     * @param n
+     */
+
     public static void flipNHeads(int n) {
         int countTotalFlips = 0;
         int countHeadsInARow = 0;
@@ -50,7 +63,17 @@ public class Main {
         System.out.println("It took " + countTotalFlips + " to flip " + n + " heads in a row.");
     }
 
+
+    /***************
+     * CLOCK
+     * Constantly print out the current time to the console
+     */
     public static void clock() {
-        System.out.println("Allo from clock!");
+        // create a LocalDateTime object
+        LocalDateTime current = LocalDateTime.now();
+
+        String time = current.format(DateTimeFormatter.ofPattern("HH:mm:ss"));
+        // loop displaying the updated time each second.
+        System.out.println(time);
     }
 }
