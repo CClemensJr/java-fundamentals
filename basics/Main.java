efeb0db0fbf25a31d501929fe028709d6f1c1580
Charles Clemens <cclemensjr@gmail.com>
2019-05-28 09:20:19 -0700

    Added method sig for pluralize

--- basics/Main.java
+++ basics/Main.java
@@ -1,10 +1,17 @@
-import java.time.localDateTime;
+//import java.time.localDateTime;
 
 public class Main {
     public static void main(String[] args) {
         //call pluralize
         //call flipNHeads
         //call clock
+
+        System.out.println("Allo, love!");
     }
 
+    public static String pluralize(String word, int number) {
+        System.out.println("Allo from pluralize!");
+
+        return "Allo";
+    }
 }
