2999f43279033dde10e3c2b255b14aa6a837efba
Charles Clemens <cclemensjr@gmail.com>
2019-06-04 10:35:57 -0700

    Tested default constructor

--- inheritance/src/test/java/inheritance/RestaurantTest.java
+++ inheritance/src/test/java/inheritance/RestaurantTest.java
@@ -1,4 +1,13 @@
 package inheritance;
 
+import org.junit.Test;
+import static org.junit.Assert.*;
+
 public class RestaurantTest {
+    @Test
+    public void canCreateRestaurantWithDefaultConstructor() {
+        Restaurant testerant = new Restaurant("The Testerant Restaurant");
+
+        assertTrue(testerant.getName() == "The Testerant Restaurant");
+    }
 }
