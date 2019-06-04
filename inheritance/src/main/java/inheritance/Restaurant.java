c0b552faf7504c86cc075720710a7e43d2ed8204
Charles Clemens <cclemensjr@gmail.com>
2019-06-04 10:31:16 -0700

    Added properties, constructors, and getters/setters

--- inheritance/src/main/java/inheritance/Restaurant.java
+++ inheritance/src/main/java/inheritance/Restaurant.java
@@ -1,4 +1,39 @@
 package inheritance;
 
 public class Restaurant {
+/*********
+ * Instance Variables
+ * */
+    private String name;
+    private int numberOfStars;
+    private int priceCategory;
+
+
+/*********
+ * Constructors
+ * */
+    public Restaurant(String name) {
+        this.name = name;
+        this.numberOfStars = 0;
+        this.priceCategory = 0;
+    }
+
+    public Restaurant(String name, int numberOfStars, int priceCategory) {
+        this.name = name;
+        this.numberOfStars = numberOfStars;
+        this.priceCategory = priceCategory;
+    }
+
+
+/*********
+ * Getters and Setters
+ * */
+    public String getName() { return this.name; }
+    public int getNumberOfStars() { return this.numberOfStars; }
+    public int getPriceCategory() { return this.priceCategory; }
+
+    public void setName(String name) { this.name = name; }
+    public void setNumberOfStars(int numberOfStars) { this.numberOfStars = numberOfStars; }
+    public void setPriceCategory(int priceCategory) { this.priceCategory = priceCategory; }
+
 }
