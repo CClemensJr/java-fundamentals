096d8de5fe8d5b2f8abba66a0dfd41d8255821a0
Charles Clemens <cclemensjr@gmail.com>
2019-06-04 10:37:38 -0700

    Tested custom constructor

--- inheritance/src/test/java/inheritance/RestaurantTest.java
+++ inheritance/src/test/java/inheritance/RestaurantTest.java
@@ -10,4 +10,11 @@ public class RestaurantTest {
 
         assertTrue(testerant.getName() == "The Testerant Restaurant");
     }
+
+    @Test
+    public void canCreateRestaurantWithCustomConstructor() {
+        Restaurant testerant = new Restaurant("The Testerant Restaurant", 4, 2);
+
+        assertTrue(testerant.getNumberOfStars() == 4);
+    }
 }
