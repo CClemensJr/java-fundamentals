89c067eb2706586bd9981789d7b128c5a06140a6
Charles Clemens <cclemensjr@gmail.com>
2019-06-04 11:38:16 -0700

    Tested complete reviews

--- inheritance/src/test/java/inheritance/ReviewTest.java
+++ inheritance/src/test/java/inheritance/ReviewTest.java
@@ -10,4 +10,16 @@ public class ReviewTest {
 
         assertTrue(test.getNumberOfStars() == 1);
     }
+
+    @Test
+    public void canCreateACompleteReview() {
+        String testBody = "There is a lot of stuff I could say about this restaurant but I probably won't because I did " +
+                          "not want to create a full review because that would take forever.";
+        String testAuthor = "Chandy La\'Twillian";
+        int testRating = 5;
+
+        Review test = new Review(testBody, testAuthor, testRating);
+
+        assertTrue(test.getAuthor() == testAuthor);
+    }
 }
