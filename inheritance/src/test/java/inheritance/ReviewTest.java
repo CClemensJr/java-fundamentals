db35ab4baab7be76c06fbbceb99dc4d24dcd7cc4
Charles Clemens <cclemensjr@gmail.com>
2019-06-04 11:33:36 -0700

    Tested empty reviews

--- inheritance/src/test/java/inheritance/ReviewTest.java
+++ inheritance/src/test/java/inheritance/ReviewTest.java
@@ -1,4 +1,13 @@
 package inheritance;
 
+import org.junit.Test;
+import static org.junit.Assert.*;
+
 public class ReviewTest {
+    @Test
+    public void canCreateAnEmptyReview() {
+        Review test = new Review();
+
+        assertTrue(test.getNumberOfStars() == 1);
+    }
 }
