ae8fc1df0de5fe8c36a35ba97c2c32cb3c5a593d
Charles Clemens <cclemensjr@gmail.com>
2019-06-04 10:50:56 -0700

    Implemented toString method

--- inheritance/src/main/java/inheritance/Restaurant.java
+++ inheritance/src/main/java/inheritance/Restaurant.java
@@ -6,7 +6,7 @@ public class Restaurant {
  * */
     private String name;
     private int numberOfStars;
-    private int priceCategory;
+    private String priceCategory;
 
 
 /*********
@@ -15,10 +15,10 @@ public class Restaurant {
     public Restaurant(String name) {
         this.name = name;
         this.numberOfStars = 0;
-        this.priceCategory = 0;
+        this.priceCategory = "$";
     }
 
-    public Restaurant(String name, int numberOfStars, int priceCategory) {
+    public Restaurant(String name, int numberOfStars, String priceCategory) {
         this.name = name;
         this.numberOfStars = numberOfStars;
         this.priceCategory = priceCategory;
@@ -30,10 +30,18 @@ public class Restaurant {
  * */
     public String getName() { return this.name; }
     public int getNumberOfStars() { return this.numberOfStars; }
-    public int getPriceCategory() { return this.priceCategory; }
+    public String getPriceCategory() { return this.priceCategory; }
 
     public void setName(String name) { this.name = name; }
     public void setNumberOfStars(int numberOfStars) { this.numberOfStars = numberOfStars; }
-    public void setPriceCategory(int priceCategory) { this.priceCategory = priceCategory; }
+    public void setPriceCategory(String priceCategory) { this.priceCategory = priceCategory; }
+
+
+/*********
+ * Instance Methods
+ * */
+    public String toString() {
+        return String.format("Name: %a | Rating: %b | Price: %c", this.name, this.numberOfStars, this.priceCategory);
+    }
 
 }
