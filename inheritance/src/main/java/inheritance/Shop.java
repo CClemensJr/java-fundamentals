b8b6b2927bcedac5cfc28d69bc5ef2d84543629c
Charles Clemens <cclemensjr@gmail.com>
2019-06-05 10:19:28 -0700

    Set default constructor

--- inheritance/src/main/java/inheritance/Shop.java
+++ inheritance/src/main/java/inheritance/Shop.java
@@ -1,4 +1,31 @@
 package inheritance;
 
 public class Shop {
+/*********
+ * Instance Variables
+ * */
+    private String name;
+    private String description;
+    private String priceRating;
+
+/*********
+ * Constructors
+ * */
+    public Shop() {
+        this.name = "Please set the shop name";
+        this.description = "Please set the shop description.";
+        this.priceRating = "Please set the price rating.";
+    }
+
+
+
+/*********
+ * Getters and Setters
+ * */
+
+
+/*********
+ * Instance Methods
+ * */
+
 }
