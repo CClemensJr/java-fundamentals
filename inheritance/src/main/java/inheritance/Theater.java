d7e51cacb6ac10f76396ec910aa52a5e4f7618d1
Charles Clemens <cclemensjr@gmail.com>
2019-06-05 11:20:24 -0700

    Added constructor

--- inheritance/src/main/java/inheritance/Theater.java
+++ inheritance/src/main/java/inheritance/Theater.java
@@ -1,4 +1,31 @@
 package inheritance;
 
+import java.util.List;
+
 public class Theater {
+/*********
+ * Instance Variables
+ * */
+    private String name;
+    private List<String> movies;
+
+
+/*********
+ * Constructors
+ * */
+    public Theater(String name) {
+        this.name = name;
+    }
+
+
+/*********
+ * Getters and Setters
+ * */
+
+
+
+/*********
+ * Instance Methods
+ * */
+
 }
